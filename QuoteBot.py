7541306b91bf6de499e5ce4a1578f0a484a3edfd
marcolivier rodrigue <marcolivier.rodrigue@gmail.com>
2020-04-17 13:42:12 -0400

    Chameless plug

--- QuoteBot.py
+++ QuoteBot.py
@@ -7,7 +7,7 @@ import sqlite3
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 
 async def on_ready():
-    print('Im ready!')
+    return await bot.change_presence(activity=discord.Activity(type=1, name='Chameless Plug', url='https://github.com/MarcOlivierRodrigue/QuoteLibrairieDiscordBot'))
 
 
 #bot.run(TOKEN)
