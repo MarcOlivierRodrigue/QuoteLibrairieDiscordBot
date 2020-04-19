986b8efaf5442b3e7a246564de065e613187e66a
marcolivier rodrigue <marcolivier.rodrigue@gmail.com>
2020-04-19 15:00:45 -0400

    add and help commands in dev

--- QuoteBot.py
+++ QuoteBot.py
@@ -6,22 +6,61 @@ import sqlite3
 import datetime
 
 
-client = commands.Bot(command_prefix='!quote', case_insensitive=True)
+client = commands.Bot(command_prefix='!', case_insensitive=True)
+client.remove_command('help')
+
+def getHelpText():
+    helpMessage = "Hi, I'm **Quotebot**! Here to store any ~~goofs or~~ important informations going on here at your leasure. 😎\n\n -  **Get a reminder:** `!help`\n\n -  **Add a new quote:** `!add title- content- author-` \n\n -  **Display a quote:** `!show title`\n\n -  **Display a random quote:** `!random`\n\n  -  **Delete a quote:** `!delete title`\n\n **Note:** `title`, `content` and `author` are defined by you when adding a new quote."
+    embed = discord.Embed(title="Github", colour=0xecb656, url="https://github.com/MarcOlivierRodrigue/QuoteLibrairieDiscordBot", description="The goal of this discord bot is to allow user of guild to manualy register quotes as value and the user as Key. So that they can display later as reminders or funny goofs. You can add it to your guild with this [link](https://discordapp.com)")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.set_author(name="By Marc-Olivier Rodrigue", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.set_footer(text="You can do whatever you want with the source code", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+    embed.timestamp = datetime.datetime.utcnow()
+    return [helpMessage , embed]
+
 
 @client.event
 async def on_ready():
     print("Quotebot ready!")
 
 @client.event
-async def on_guild_join(server):
-    for channel in server.text_channels:
-        if channel.permissions_for(server.me).send_messages:
-            embed = discord.Embed(title="Github", colour=0xecb656, url="https://github.com/MarcOlivierRodrigue/QuoteLibrairieDiscordBot", description="The goal of this discord bot is to allow user of server to manualy register quotes as value and the user as Key. So that they can display later as reminders or funny goofs. You can add it to your server with this [link](https://discordapp.com)")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
-            embed.set_author(name="By Marc-Olivier Rodrigue", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
-            embed.set_footer(text="You can do whatever you want with the source code", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
-            embed.timestamp = datetime.datetime.utcnow()
+async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            entryTexts = getHelpText()
+            await channel.send(content=entryTexts[0], embed=entryTexts[1])
 
-            await channel.send(content="Hi, I'm **Quotebot**! Here to store any ~~goofs or~~ important informations going on here at your leasure. 😎\n\n -  **Get a reminder:** `!help`\n\n -  **Add a new quote:** `!add title quote author` \n\n -  **Display a quote:** `!show title`\n\n -  **Display a random quote:** `!random`\n\n  -  **Delete a quote:** `!delete title`\n\n **Note:** `title`, `content` and `author` are defined by you when adding a new quote.", embed=embed)
+
+
+@client.command()
+async def add(ctx, *args: str):
+    paramCounter = 0
+    params = ["", "", ""]
+    #//////////
+    #TODO: Make sure that the comment only has 3 parameters
+    #//////////
+    for arg in args:
+        if paramCounter > 2:
+            break 
+        else:
+            if(arg.endswith('-')):
+                lastWord = arg.rstrip('-')
+                params[paramCounter] +=  (lastWord + " ")
+                paramCounter += 1
+                continue
+            params[paramCounter] +=  (arg + " ")
+
+    message = "**New quote** {} \n *\"{}\"* \n- {} \n added by {}"
+    #//////////
+    #TODO: Add the quote in the sqlite table
+    #//////////
+    await ctx.send(message.format(params[0], params[1], params[2], ctx.author.name))
+
+
+@client.command()
+async def help(ctx):
+    helpText = getHelpText()
+    await ctx.channel.send(content=helpText[0], embed=helpText[1])
+
 
 #client.run('TOKEN')
