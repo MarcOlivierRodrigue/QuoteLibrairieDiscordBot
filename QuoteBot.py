0ec4c903bf6242e3312c4d7a88a9990f91cd796c
marcolivier rodrigue <marcolivier.rodrigue@gmail.com>
2020-04-20 15:48:26 -0400

    Fixed add commend

--- QuoteBot.py
+++ QuoteBot.py
@@ -10,7 +10,7 @@ client = commands.Bot(command_prefix='!', case_insensitive=True)
 client.remove_command('help')
 
 def getHelpText():
-    helpMessage = "Hi, I'm **Quotebot**! Here to store any ~~goofs or~~ important informations going on here at your leasure. 😎\n\n -  **Get a reminder:** `!help`\n\n -  **Add a new quote:** `!add title- content- author-` \n\n -  **Display a quote:** `!show title`\n\n -  **Display a random quote:** `!random`\n\n  -  **Delete a quote:** `!delete title`\n\n **Note:** `title`, `content` and `author` are defined by you when adding a new quote."
+    helpMessage = "Hi, I'm **Quotebot**! Here to store any ~~goofs or~~ important informations going on here at your leasure. 😎\n\n -  **Get a reminder:** `!help`\n\n -  **Add a new quote:** `!add \"title\" \"content\" \"author\"` \n\n -  **Display a quote:** `!show \"title\"`\n\n -  **Display a random quote:** `!random`\n\n  -  **Delete a quote:** `!delete \"title\"`\n\n **Note:** `title`, `content` and `author` are defined by you when adding a new quote."
     embed = discord.Embed(title="Github", colour=0xecb656, url="https://github.com/MarcOlivierRodrigue/QuoteLibrairieDiscordBot", description="The goal of this discord bot is to allow user of guild to manualy register quotes as value and the user as Key. So that they can display later as reminders or funny goofs. You can add it to your guild with this [link](https://discordapp.com)")
     embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
     embed.set_author(name="By Marc-Olivier Rodrigue", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
@@ -33,28 +33,22 @@ async def on_guild_join(guild):
 
 
 @client.command()
-async def add(ctx, *args: str):
-    paramCounter = 0
-    params = ["", "", ""]
-    #//////////
-    #TODO: Make sure that the comment only has 3 parameters
-    #//////////
+async def add(ctx, *args):
+    validArgs = True
+    paramCount = 0
     for arg in args:
-        if paramCounter > 2:
-            break 
-        else:
-            if(arg.endswith('-')):
-                lastWord = arg.rstrip('-')
-                params[paramCounter] +=  (lastWord + " ")
-                paramCounter += 1
-                continue
-            params[paramCounter] +=  (arg + " ")
+        paramCount += 1 
+        if type(arg) != str:
+            validArgs = False
 
-    message = "**New quote** {} \n *\"{}\"* \n- {} \n added by {}"
-    #//////////
-    #TODO: Add the quote in the sqlite table
-    #//////////
-    await ctx.send(message.format(params[0], params[1], params[2], ctx.author.name))
+    if paramCount == 3 and validArgs:
+        message = "**New quote** {} \n *\"{}\"* \n- {} \n added by {}"
+        #//////////
+        #TODO: Add the quote in the sqlite table
+        #//////////
+        await ctx.send(message.format(args[0], args[1], args[2], ctx.author.name))
+    else: 
+         await ctx.send("Error: This command is invalid. Please make sure each parameter has \" around it")
 
 
 @client.command()
